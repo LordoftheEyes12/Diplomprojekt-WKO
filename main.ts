f8af0f1586202f9c3fc5c106852b470842c3d964
Ben Daschner <ben@daschner.at>
2024-12-20 09:32:17 +0100

    added logo

--- main.ts
+++ main.ts
@@ -11,9 +11,7 @@ const DataBase = new DB('dev.db');
 
 
 const schema = "CREATE TABLE `categories` ( `CategoryID` INT NOT NULL UNIQUE PRIMARY KEY , `CategoryName` varchar(255) DEFAULT NULL, `Description` varchar(255) DEFAULT NULL ); CREATE TABLE `customers` ( `CustomerID` INT NOT NULL UNIQUE PRIMARY KEY, `CustomerName` varchar(255) DEFAULT NULL, `ContactName` varchar(255) DEFAULT NULL, `Address` varchar(255) DEFAULT NULL, `City` varchar(255) DEFAULT NULL, `PostalCode` varchar(255) DEFAULT NULL, `Country` varchar(255) DEFAULT NULL ); CREATE TABLE `employees` ( `EmployeeID` INT NOT NULL UNIQUE PRIMARY KEY, `LastName` varchar(255) DEFAULT NULL, `FirstName` varchar(255) DEFAULT NULL, `BirthDate` date DEFAULT NULL, `Photo` varchar(255) DEFAULT NULL, `Notes` text ); CREATE TABLE `orders` ( `OrderID` INT NOT NULL UNIQUE PRIMARY KEY, `CustomerID` INT DEFAULT NULL, `EmployeeID` INT DEFAULT NULL, `OrderDate` date DEFAULT NULL, `ShipperID` INT DEFAULT NULL, FOREIGN KEY (`CustomerID`) REFERENCES `customers` (`CustomerID`), FOREIGN KEY (`EmployeeID`) REFERENCES `employees` (`EmployeeID`), FOREIGN KEY (`ShipperID`) REFERENCES `shippers` (`ShipperID`) ); CREATE TABLE `order_details` ( `OrderDetailID` INT NOT NULL UNIQUE PRIMARY KEY, `OrderID` INT DEFAULT NULL, `ProductID` INT DEFAULT NULL, `Quantity` INT DEFAULT NULL, FOREIGN KEY (`OrderID`) REFERENCES `orders` (`OrderID`), FOREIGN KEY (`ProductID`) REFERENCES `products` (`ProductID`) ); CREATE TABLE `products` ( `ProductID` INT NOT NULL UNIQUE PRIMARY KEY, `ProductName` varchar(255) DEFAULT NULL, `SupplierID` INT DEFAULT NULL, `CategoryID` INT DEFAULT NULL, `Unit` varchar(255) DEFAULT NULL, `Price` double DEFAULT NULL, FOREIGN KEY (`CategoryID`) REFERENCES `categories` (`CategoryID`), FOREIGN KEY (`SupplierID`) REFERENCES `suppliers` (`SupplierID`) ); CREATE TABLE `shippers` ( `ShipperID` INT NOT NULL UNIQUE PRIMARY KEY, `ShipperName` varchar(255) DEFAULT NULL, `Phone` varchar(255) DEFAULT NULL ); CREATE TABLE `suppliers` ( `SupplierID` INT NOT NULL UNIQUE PRIMARY KEY, `SupplierName` varchar(255) DEFAULT NULL, `ContactName` varchar(255) DEFAULT NULL, `Address` varchar(255) DEFAULT NULL, `City` varchar(255) DEFAULT NULL, `PostalCode` varchar(255) DEFAULT NULL, `Country` varchar(255) DEFAULT NULL, `Phone` varchar(255) DEFAULT NULL );";
-console.log(schema);
 
-//const expression = "(?i)^\s*SELECT\b[\s\S]*?(?:\bFROM\b[\s\S]*?;|\bFROM\b[\s\S]*?$)";
 
 const routes: Route[] = [
   {
@@ -24,7 +22,7 @@ const routes: Route[] = [
         return new Response(null);
       }
       if (req.method === "GET") {
-        // Extract query parameters
+      
         const url = new URL(req.url);
         const input = url.searchParams.get("Input");
         
@@ -44,7 +42,7 @@ const routes: Route[] = [
         return new Response(null);
       }
       if (req.method === "GET") {
-        // Extract query parameters
+     
         const url = new URL(req.url);
         
         let model = url.searchParams.get("Model");
@@ -72,7 +70,7 @@ const routes: Route[] = [
         return new Response(null);
       }
       if (req.method === "GET") {
-        // Extract query parameters
+     
         const html = await Deno.readFile("./public/index.html");
         return new Response(html, { headers: { "Content-Type": "text/html" } });
       }
@@ -88,9 +86,25 @@ const routes: Route[] = [
         return new Response(null);
       }
       if (req.method === "GET") {
-        // Extract query parameters
+    
         const html = await Deno.readFile("./public/new.html");
         return new Response(html, { headers: { "Content-Type": "text/html" } });
+      }
+
+      return new Response("Method not allowed", { status: 405 });
+    },
+  },
+  {
+    method: ["GET", "HEAD"],
+    pattern: new URLPattern({ pathname: "/logo.png" }),
+    handler: async (req: Request) => {
+      if (req.method === "HEAD") {
+        return new Response(null);
+      }
+      if (req.method === "GET") {
+        
+        const html = await Deno.readFile("./public/logo.png");
+        return new Response(html, { headers: { "Content-Type": "image/png" } });
       }
 
       return new Response("Method not allowed", { status: 405 });
@@ -104,7 +118,7 @@ async function apiCall(input: string | null) {
   const daten = DataBase.query(result || "");
   const mdTable =await getMarkdownTable(daten.toString() , result) ;
   return JSON.stringify({ input, result, daten, mdTable });
-  //return JSON.stringify({ data: "API response", input, result });
+  
 }
 
 async function getSQLQuery(input: string | null) {
